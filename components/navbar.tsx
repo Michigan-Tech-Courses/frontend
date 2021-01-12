500718e9f5371df10e8835524edf8d2d08b05234
Max Isom <hi@maxisom.me>
2021-01-12 18:09:20 -0500

    Make navbar responsive

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import {HStack, Select, Spacer} from '@chakra-ui/react';
+import React, {useState, useCallback} from 'react';
+import {Flex, Box, Select, IconButton, Heading, HStack} from '@chakra-ui/react';
+import {CloseIcon, HamburgerIcon} from '@chakra-ui/icons';
 import ColorModeToggle from './color-mode-toggle';
 import Link from './link';
 
+const PAGES = [
+	{
+		label: 'Courses',
+		href: '/'
+	},
+	{
+		label: 'Transfer Courses',
+		href: '/transfer'
+	}
+];
+
 const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const handleToggle = useCallback(() => {
+		setIsOpen(o => !o);
+	}, []);
+
 	return (
-		<HStack w="100%" p="1rem" as="nav">
-			<Link href="/" mr="1rem">Michigan Tech Courses</Link>
+		<Flex align="center" justify="space-between" wrap="wrap" p="1rem" as="nav" mb={8}>
+			<Flex align="center" mr={5}>
+				<Heading as="h1" size="lg">
+          Courses @ Tech
+				</Heading>
+			</Flex>
 
-			<Link href="/transfer">Transfer Courses</Link>
+			<Box display={{base: 'block', md: 'none'}} onClick={handleToggle} ml="auto">
+				<IconButton aria-label={isOpen ? 'Close navbar' : 'Open navbar'}>
+					{isOpen ? <CloseIcon/> : <HamburgerIcon/>}
+				</IconButton>
+			</Box>
 
-			<Spacer/>
+			<Box
+				display={{base: isOpen ? 'block' : 'none', md: 'flex'}}
+				width={{base: 'full', md: 'auto'}}
+				alignItems="center"
+				flexGrow={1}>
+				{
+					PAGES.map(page => (
+						<Link key={page.href} href={page.href} display="block" mr={6} mt={{base: 4, md: 0}}>{page.label}</Link>
+					))
+				}
+			</Box>
 
-			<Select w="auto" variant="filled" aria-label="Select a semester to view">
-				<option>Fall 2020</option>
-			</Select>
+			<HStack
+				display={{base: isOpen ? 'flex' : 'none', md: 'flex'}}
+				mt={{base: 4, md: 0}}
+			>
+				<Select w="auto" variant="filled" aria-label="Select a semester to view">
+					<option>Fall 2020</option>
+				</Select>
 
-			<ColorModeToggle/>
-		</HStack>
+				<ColorModeToggle/>
+
+			</HStack>
+		</Flex>
 	);
 };
 
