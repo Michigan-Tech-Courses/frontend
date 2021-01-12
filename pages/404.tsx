8bebf7a01c3946dfbc4dffb9ce3f79c408852998
Max Isom <hi@maxisom.me>
2021-01-12 17:25:47 -0500

    Add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container, Text} from '@chakra-ui/react';
+
+const NotFoundPage = () => (
+	<Container>
+		<Text fontSize="2xl" fontWeight="bold" textAlign="center">Page not found.</Text>
+	</Container>
+);
+
+export default NotFoundPage;
