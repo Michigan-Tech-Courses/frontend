77705c78f1592253d42666e208b613aaa66350f4
Max Isom <hi@maxisom.me>
2021-01-12 12:19:55 -0500

    Fix warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import type {AppProps} from 'next/app';
+import Head from 'next/head';
 import {ChakraProvider} from '@chakra-ui/react';
 import Navbar from '../components/navbar';
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
 		<ChakraProvider>
+			<Head>
+				<title>Michigan Tech Courses</title>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			</Head>
 			<Navbar/>
 
 			<Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,7 @@ export default class CustomDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head>
-					<title>Michigan Tech Courses</title>
-					<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-				</Head>
+				<Head/>
 
 				<body>
 					<ColorModeScript initialColorMode="light" />
