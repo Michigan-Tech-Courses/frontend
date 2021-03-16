8f8a09e0555a3b9e24275184f241bc45bb0dd2f3
Max Isom <hi@maxisom.me>
2021-03-16 18:18:55 -0400

    Fix config?

--- next.config.js
+++ next.config.js
@@ -8,7 +8,33 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withPlugins([
 	[withBundleAnalyzer],
-	[withOffline, ['!', PHASE_DEVELOPMENT_SERVER]]
+	[withOffline, {
+		workboxOpts: {
+			swDest: process.env.NEXT_EXPORT ?
+				'service-worker.js' :
+				'static/service-worker.js',
+			runtimeCaching: [
+				{
+					urlPattern: /^https?.*/,
+					handler: 'NetworkFirst',
+					options: {
+						cacheName: 'offlineCache',
+						expiration: {
+							maxEntries: 200
+						}
+					}
+				}
+			]
+		},
+		async rewrites() {
+			return [
+				{
+					source: '/service-worker.js',
+					destination: '/_next/static/service-worker.js'
+				}
+			];
+		}
+	}, ['!', PHASE_DEVELOPMENT_SERVER]]
 ], {
 	webpack: config => {
 		config.module.rules.push({
