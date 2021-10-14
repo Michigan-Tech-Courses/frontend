6b499418022c323c23cd6b9528a50ff0eede4a14
Max Isom <hi@maxisom.me>
2021-10-13 21:16:13 -0400

    Add warning for mobile devices

---
+++ src/components/mobile-device-warning.tsx
@@ -0,0 +1,46 @@
+import React, {useEffect} from 'react';
+import {
+	Button,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+	useDisclosure,
+} from '@chakra-ui/react';
+import Bowser from 'bowser';
+
+const MobileDeviceWarning = () => {
+	const {isOpen, onClose, onOpen} = useDisclosure();
+
+	useEffect(() => {
+		const browser = Bowser.getParser(window.navigator.userAgent);
+
+		if (browser.getPlatform().type !== 'desktop') {
+			onOpen();
+		}
+	}, [onOpen]);
+
+	return (
+		<Modal isOpen={isOpen} onClose={onClose}>
+			<ModalOverlay/>
+			<ModalContent>
+				<ModalHeader>Warning</ModalHeader>
+				<ModalCloseButton/>
+				<ModalBody>
+					This site is primarily made for laptop and desktop use. There's just too much information to effectively display it on mobile devices.
+				</ModalBody>
+
+				<ModalFooter>
+					<Button colorScheme="blue" mr={3} onClick={onClose}>
+						I understand
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	);
+};
+
+export default MobileDeviceWarning;
