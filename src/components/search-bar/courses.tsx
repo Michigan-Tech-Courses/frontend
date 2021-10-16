5a852a11cc174dbf9ab8c43b47d0c1c25f7b1928
Max Isom <hi@maxisom.me>
2021-10-15 22:11:36 -0400

    Disable add search query to basket if no current basket

--- src/components/search-bar/courses.tsx
+++ src/components/search-bar/courses.tsx
@@ -179,6 +179,7 @@ const CoursesSearchBar = observer(() => {
 						rounded="full"
 						size="xs"
 						mr={2}
+						isDisabled={!currentBasket}
 						onClick={handleQuerySaveOrDelete}
 					/>
 				</Tooltip>
