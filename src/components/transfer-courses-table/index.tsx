a134f3ff4bd6ceda68d6c189ad57870a2a48b529
Crxmsxn-git <124933939+Crxmsxn-git@users.noreply.github.com>
2024-10-23 22:02:07 -0400

    Widen transfer course table

--- src/components/transfer-courses-table/index.tsx
+++ src/components/transfer-courses-table/index.tsx
@@ -51,7 +51,7 @@ const TransferCoursesTable = observer(({onScrollToTop}: {onScrollToTop: () => vo
 	}, [store.transferCoursesState.filteredCourses.length, setPage]);
 
 	return (
-		<VStack w='min(100rem, 80%)'>
+		<VStack w='min(100rem, 90%)'>
 			<DataFilterStatsBar
 				isLoaded={store.transferCoursesState.hasData}
 				matched={store.transferCoursesState.filteredCourses.length.toLocaleString()}
